d1ff28fa2c14c8d971ccc4d52a18a3bcfb3d0512
almogvc <almogvc@gmail.com>
2019-12-20 20:52:51 +0200

    refactor(host service): add get one function

--- src/hostService/hostService.repository.ts
+++ src/hostService/hostService.repository.ts
@@ -5,4 +5,8 @@ export default class HostServiceRepository {
     public static create(hostService: IHostService) {
         return HostServiceModel.create(hostService);
     }
+
+    public static getOne(service: string, hostname: string) {
+        return HostServiceModel.findOne({ service, hostname });
+    }
 }

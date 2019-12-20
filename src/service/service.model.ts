4f59b5c954b6f28c1e237f92afe1cdb77b8f41fa
almogvc <almogvc@gmail.com>
2019-12-20 20:49:43 +0200

    refactor(service): add mongoose model

---
+++ src/service/service.model.ts
@@ -0,0 +1,49 @@
+import * as mongoose from 'mongoose';
+import IService from './service.interface';
+
+const serviceSchema: mongoose.Schema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            immutable: true,
+        },
+        lastContactDate: {
+            type: Date,
+            required: true,
+        },
+        longestSilentPeriodInSeconds: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        longestAlivePeriodInSeconds: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        currentAlivePeriodInSeconds: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
+    {
+        versionKey: false,
+        autoIndex: false,
+        timestamps: { createdAt: true, updatedAt: true },
+        id: true,
+        toJSON: {
+            virtuals: true,
+            transform: (doc, ret) => {
+                delete ret._id;
+            },
+        },
+    },
+);
+
+serviceSchema.index({ name: 1 });
+const ServiceModel = mongoose.model<IService & mongoose.Document>('Service', serviceSchema);
+
+export default ServiceModel;

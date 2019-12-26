0d394897c9014c74380513a7095697c25d8b9880
almogvc <almogvc@gmail.com>
2019-12-26 21:23:18 +0200

    refactor(alive signal controller): use deconstruct in create function

--- src/aliveSignal/aliveSignal.controller.ts
+++ src/aliveSignal/aliveSignal.controller.ts
@@ -1,16 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
-import IAliveSignal from './aliveSignal.interface';
 import AliveSignalManager from './aliveSignal.manager';
 
 export default class AliveSignalController {
     public static async create(req: Request, res: Response, next: NextFunction) {
-        const aliveSignal: IAliveSignal = {
-            hostname: req.body.hostname,
-            serviceName: req.body.serviceName,
-            aliveDate: req.body.aliveDate,
-            upTimeInSeconds: req.body.upTimeInSeconds,
-        };
+        const { hostname, serviceName, aliveDate, upTimeInSeconds } = req.body;
 
-        res.json(await AliveSignalManager.create(aliveSignal));
+        res.json(await AliveSignalManager.create({ hostname, serviceName, aliveDate, upTimeInSeconds }));
     }
 }

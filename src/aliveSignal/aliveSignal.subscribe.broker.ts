7588c44fda614b17eb9842c0ed98e834651ca7f2
almogvc <almogvc@gmail.com>
2019-12-28 15:22:40 +0200

    refactor: add validation to rabbitmq messages

--- src/aliveSignal/aliveSignal.subscribe.broker.ts
+++ src/aliveSignal/aliveSignal.subscribe.broker.ts
@@ -1,6 +1,12 @@
 import * as rabbit from '../utils/rabbit';
 import config from '../config';
 import AliveSignalManager from './aliveSignal.manager';
+import {
+    validateServiceName,
+    validateHostname,
+    validateAliveDate,
+    validateUpTimeInSeconds,
+} from './validators/aliveSignal.validator';
 
 export default class AliveSignalSubscribeBroker {
     public static async subscribe() {
@@ -11,8 +17,14 @@ export default class AliveSignalSubscribeBroker {
             config.rabbitMQ.pattern,
             async messageContent => {
                 const { hostname, serviceName, aliveDate, upTimeInSeconds } = messageContent;
-
-                await AliveSignalManager.create({ hostname, serviceName, aliveDate, upTimeInSeconds });
+                if (
+                    validateHostname(hostname) === undefined &&
+                    validateServiceName(serviceName) === undefined &&
+                    validateAliveDate(aliveDate) === undefined &&
+                    validateUpTimeInSeconds(upTimeInSeconds) === undefined
+                ) {
+                    await AliveSignalManager.create({ hostname, serviceName, aliveDate, upTimeInSeconds });
+                }
             },
         );
     }
